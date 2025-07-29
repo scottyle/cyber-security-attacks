3eafdde6f6693b82c4e20a74bd5710d9a5f272b4
Scott Le <scottyle02@outlook.com>
2025-07-29 18:27:30 -0400

    Added TODO for data cleaning

--- main.py
+++ main.py
@@ -22,6 +22,39 @@ print(f"Length of the dataset: {len(data)}")
 
 # Check for missing values 
 print(data.isnull().sum())
+"""
+1. Create a function to clean the data 
+ a. For malware indicators, replace NaN with 'No Malware Indicators'
+ b. For alerts/warnings, replace NaN with 'Warning'
+ c. For proxy information, if Destination IP Address is a private IP address, replace with NaN, all others replace with 'No Proxy Information'
+ 
+Missing values in the dataset 
+Timestamp                     0
+Source IP Address             0
+Destination IP Address        0
+Source Port                   0
+Destination Port              0
+Protocol                      0
+Packet Length                 0
+Packet Type                   0
+Traffic Type                  0
+Payload Data                  0
+Malware Indicators        20000
+Anomaly Scores                0
+Alerts/Warnings           20067
+Attack Type                   0
+Attack Signature              0
+Action Taken                  0
+Severity Level                0
+User Information              0
+Device Information            0
+Network Segment               0
+Geo-location Data             0
+Proxy Information         19851
+Firewall Logs             19961
+IDS/IPS Alerts            20050
+Log Source                    0
+"""
 
 print(data.head())  # Display the first few rows of the dataset
 
